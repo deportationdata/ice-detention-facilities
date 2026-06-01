7172f64ef38d21e48559e48a459130599e5e3692
Graeme Blair <graemeblair@users.noreply.github.com>
2026-06-01 08:06:45 -0700

    add district and circuit to final file

--- code/8-format-dataset.R
+++ code/8-format-dataset.R
@@ -295,6 +295,8 @@ facility_formatted <-
     latitude,
     longitude,
     field_office,
+    federal_court_district,
+    federal_court_circuit,
     days_with_detentions_daily_last_year,
     days_with_detentions_midnight_last_year,
     average_daily_population_last_year,
@@ -333,10 +335,14 @@ facility_formatted_sf <-
     remove = FALSE
   )
 
+stop()
 
-sfarrow::st_write_parquet(
+unlink("data/facilities-latest-sf.parquet")
+st_write(
   facility_formatted_sf,
-  "data/facilities-latest-sf.parquet"
+  "data/facilities-latest-sf.parquet",
+  driver = "Parquet",
+  delete_dsn = TRUE
 )
 
 temp_dir <- tempdir()
@@ -367,25 +373,3 @@ zip(
   ),
   flags = "-j"
 )
-
-# # metadata for diagnostics
-# best_values_metadata <-
-#   best_values |>
-#   select(
-#     detention_facility_code,
-#     variable,
-#     n_changes,
-#     n_unique,
-#     has_reversion,
-#     is_aba,
-#     review_flag
-#   ) |>
-#   pivot_wider(
-#     names_from = variable,
-#     values_from = c(n_changes, n_unique, has_reversion, is_aba, review_flag),
-#     names_glue = "{variable}_{.value}"
-#   )
-
-# facility_final_metadata <-
-#   facility_final |>
-#   left_join(best_values_metadata, by = "detention_facility_code")
